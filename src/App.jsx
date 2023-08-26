3a40754144c72416e47a12b9f1e019bb95909d4e
Gavutham <kgavutham2005@gmail.com>
2023-08-26 12:51:36 +0530

    initiate basic pages and initialize react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,37 @@
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Navbar from "./components/Navbar/Navbar";
+import Home from "./pages/Home/Home";
+import About from "./pages/About/About";
+import Contact from "./pages/Contact/Contact";
+import NotFound from "./pages/Not Found/NotFound";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Navbar />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/about",
+        element: <About />,
+      },
+      {
+        path: "/contact",
+        element: <Contact />,
+      },
+    ],
+  },
+  {
+    path: "*",
+    element: <NotFound />,
+  },
+]);
+
 function App() {
-  return <h1>Hello World!</h1>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
